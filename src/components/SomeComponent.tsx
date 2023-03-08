92e6ce879dc433c4d0cf1ee495d32c70e24145fb
Code Different <code.diff3rent@gmail.com>
2023-03-08 12:03:20 +1100

    First change

--- src/components/SomeComponent.tsx
+++ src/components/SomeComponent.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 // * Traditional green comment
 // ? What the hell am I doing?
 function SomeComponent() {
-    return <p>SomeComponent</p>
+    return <p>Some change</p>
 }
 
 export default SomeComponent
